a3dab15748885529ef93ac06966fd62e1e774915
duanye2006 <285712334@qq.com>
2013-01-13 13:47:48 -0600

    Initial Commit

---
+++ PhotoAlbum/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PhotoAlbum
+//
+//  Created by duanye on 13-1-13.
+//  Copyright (c) 2013年 duanye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
